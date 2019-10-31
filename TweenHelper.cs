676b8f17f37347a7e76a309633cff4eb8562ad2a
MagicOrangeKKSK <452959354@qq.com>
2019-10-31 10:01:21 +0800

    一个简单的tween动画脚本

---
+++ TweenHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TweenHelper 
+{
+    public static IEnumerator MoveTo(Transform transform, Vector3 start, Vector3 end, float time, Action backCall = null)
+    {
+        yield return TimeTo(time, t => { transform.position = Vector3.Lerp(start, end, t); }, backCall);
+    }
+
+    public static IEnumerator SizeTo(Transform transform, Vector3 start, Vector3 end, float time, Action backCall = null)
+    {
+        yield return TimeTo(time, t => { transform.localScale = Vector3.Lerp(start, end, t); }, backCall);
+    }
+
+    public static IEnumerator ColorTo(SpriteRenderer sprite, Color start, Color end, float time, Action backCall = null)
+    {
+        yield return TimeTo(time, t => { sprite.color = Color.Lerp(start, end, t); }, backCall);
+    }
+
+    public static IEnumerator TimeTo(float time, Action<float> action, Action backCall = null)
+    {
+        var timer = 0f;
+        while (timer <= time)
+        {
+            timer += Time.fixedDeltaTime;
+            action?.Invoke(timer / time);
+            yield return new WaitForFixedUpdate();
+        }
+        backCall?.Invoke();
+    }
+}
